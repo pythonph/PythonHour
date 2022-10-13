1851d055cd13012b527ad420feab3cbc15459431
MrValdez <shunyvaldez@gmail.com>
2022-10-13 19:19:07 +0800

    sample02

---
+++ 001-Lets_Create_Animated_Memes_Using_Python_MoviePy/sample02.py
@@ -0,0 +1,7 @@
+from moviepy.editor import VideoFileClip, concatenate_videoclips
+
+clip1 = VideoFileClip("input/Shia LaBeouf.mp4").subclip((0, 59), (1, 2))
+clip2 = VideoFileClip("input/Shia LaBeouf.mp4").subclip(15, 19)
+
+combined = concatenate_videoclips([clip1, clip2])
+combined.write_videofile("output/example2.mp4")
